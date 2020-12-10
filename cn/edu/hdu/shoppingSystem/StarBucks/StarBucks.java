d1231d4a88138bb41e637ed23aa6e60d5e246d04
zhenghongyu <1193073442@qq.com>
2020-12-10 14:58:41 +0800

    finish starBuck module

--- cn/edu/hdu/shoppingSystem/StarBucks/StarBucks.java
+++ cn/edu/hdu/shoppingSystem/StarBucks/StarBucks.java
@@ -3,4 +3,20 @@ package edu.hdu.shoppingSystem.StarBucks;
 
 public class StarBucks {
     //just test
+    private static final String BASIC_NAME = "普通咖啡 ";
+    private static final double BASIC_PRICE = 20;
+
+    public void buy(BuyStrategy... strategies){
+        PriceServer2 server = new PriceServer2();
+        String buyInfo = BASIC_NAME;
+        double totalPrice = BASIC_PRICE;
+        for(BuyStrategy strategy:strategies){
+            String info = strategy.getInfo();
+            buyInfo = buyInfo + " " +info;
+            double price = strategy.getPrice();
+            totalPrice += price;
+        }
+        System.out.println("购买的商品信息:"+buyInfo);
+        System.out.println("总金额:"+totalPrice);
+    }
 }
