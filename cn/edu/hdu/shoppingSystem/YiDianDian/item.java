433cc3647625f6285289492e07ca151dd0806a77
“huangcaicheng” <15390733156@163.com>
2020-12-22 17:39:41 +0800

    dev-1 item

---
+++ cn/edu/hdu/shoppingSystem/YiDianDian/item.java
@@ -0,0 +1,14 @@
+package edu.hdu.shoppingSystem.yiDianDian;
+import java.util.*;
+
+public class item {
+    protected String name;
+    protected double price;
+
+    double getPrice() {
+        return price;
+    }
+    String getName() {
+        return name;
+    }
+}
